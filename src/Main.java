dba2f852674b94e653ab5051a96fe3d46b196b92
Andrey Malov <maa.pers@gmail.com>
2022-04-25 21:05:00 +0200

    If refill > 1000 added

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 500;
-        int refill = 2000;
-        boolean quoted = true;
+        int refill = 1500;
         int quote = 100;
-        int bonus = quoted ? (refill / quote) : 0;
+        int bonus;
+        if (refill > 1000) {
+            bonus = refill / quote;
+        } else {
+            bonus = 0;
+        }
         int total = balance + refill + bonus;
         System.out.println("Баланс = " + total);
         System.out.println("Начислено бонусов = " + bonus);
